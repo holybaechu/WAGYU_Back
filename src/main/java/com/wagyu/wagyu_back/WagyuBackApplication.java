82f70138528d3ead54d8fd4ae8eb8d74b247c16d
jes0131 <wkddmstn0131@gmail.com>
2025-10-15 16:30:18 +0900

    first commit

---
+++ src/main/java/com/wagyu/wagyu_back/WagyuBackApplication.java
@@ -0,0 +1,13 @@
+package com.wagyu.wagyu_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WagyuBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WagyuBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wagyu/wagyu_back/WagyuBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wagyu.wagyu_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WagyuBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
